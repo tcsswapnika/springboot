8e7bce3f30cb9a1347c5dbd850504b25aa40bf2f
tcsswapnika <tcsswapnika@gmail.com>
2021-08-25 05:37:32 +0000

    update

--- src/main/java/com/tcs/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/tcs/springbootdemo/SpringbootDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDemoApplication.class, args); //creates spring container
+		SpringApplication.run(SpringbootDemoApplication.class, args); // creates spring container
 	}
 
 }
